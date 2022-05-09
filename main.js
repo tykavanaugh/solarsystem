f64535b4ba22bda20229dd5b9582b41caa7a00c0
tykavanaugh <tykavanaugh@gmail.com>
2022-05-08 23:33:14 -0700

    planet model

--- main.js
+++ main.js
@@ -32,6 +32,7 @@ const sun = new THREE.Mesh(
 scene.add( sun );
 
 //Mercury
+let mecuryDegreePos = 90
 const mercury = new THREE.Mesh(
   new THREE.SphereGeometry( 2, 16, 8 ),
   new THREE.MeshStandardMaterial( { color: 0x484848} ),
@@ -40,6 +41,7 @@ scene.add( mercury );
 mercury.position.set(25,0,0)
 
 //Venus
+let venusDegreePos = 270
 const venus = new THREE.Mesh(
   new THREE.SphereGeometry( 3.5, 24, 16 ),
   new THREE.MeshStandardMaterial( { color: 0xb8b8b8  } ),
@@ -48,6 +50,7 @@ scene.add( venus );
 venus.position.set(40,0,0)
 
 //Earth
+let earthDegreePos = 0
 const earth = new THREE.Mesh(
   new THREE.SphereGeometry( 4, 24, 16 ),
   new THREE.MeshStandardMaterial( { color: 0x1a5276   } ),
@@ -56,6 +59,7 @@ scene.add( earth );
 earth.position.set(55,0,0)
 
 //Mars
+let marDegreePos = 300
 const mars = new THREE.Mesh(
   new THREE.SphereGeometry( 3, 24, 16 ),
   new THREE.MeshStandardMaterial( { color: 0x993d00   } ),
@@ -64,6 +68,7 @@ scene.add( mars );
 mars.position.set(70,0,0)
 
 //Jupiter
+let jupiterDegreePos = 265
 const jupiter = new THREE.Mesh(
   new THREE.SphereGeometry( 5, 24, 16 ),
   new THREE.MeshStandardMaterial( { color: 0xb07f35   } ),
@@ -72,6 +77,7 @@ scene.add( jupiter );
 jupiter.position.set(100,0,0)
 
 //Saturn
+let saturnDegreePos = 280
 const saturn = new THREE.Mesh(
   new THREE.SphereGeometry( 5, 24, 16 ),
   new THREE.MeshStandardMaterial( { color: 0xb08f36   } ),
@@ -80,6 +86,7 @@ scene.add( saturn );
 saturn.position.set(135,0,0)
 
 //Uranus
+let uranusDegreePos = 0
 const uranus = new THREE.Mesh(
   new THREE.SphereGeometry( 5, 24, 16 ),
   new THREE.MeshStandardMaterial( { color: 0x5580aa   } ),
@@ -88,12 +95,17 @@ scene.add( uranus );
 uranus.position.set(165,0,0)
 
 //Neptune
+let neptuneDegreePos = 0
 const neptune = new THREE.Mesh(
   new THREE.SphereGeometry( 5, 24, 16 ),
   new THREE.MeshStandardMaterial( { color: 0x366896  } ),
 )
 scene.add( neptune );
 neptune.position.set(195,0,0)
+
+//starry background
+const spaceTexture = new THREE.TextureLoader().load('./textures/space-stars-twinkle.gif');
+scene.background = spaceTexture;
 
 
 function moveCamera(){
@@ -102,10 +114,113 @@ function moveCamera(){
 
 // document.body.onscroll = moveCamera
 
+const convertCoord = (radius,angle) => {
+  var x = radius * Math.sin(Math.PI * 2 * angle / 360);
+  var y = radius * Math.cos(Math.PI * 2 * angle / 360);
+  return {x,y}
+};
+
+const mecuryOrbit = () => {
+  if (mecuryDegreePos === 360){
+    mecuryDegreePos = 0
+  } else {
+    mecuryDegreePos += .4
+  }
+  let coords = convertCoord(25,mecuryDegreePos)
+  mercury.position.x = coords['x']
+  mercury.position.z = coords['y']
+}
+
+const venusOrbit = () => {
+  if (venusDegreePos === 360){
+    venusDegreePos = 0
+  } else {
+    venusDegreePos += .12
+  }
+  let coords = convertCoord(40,venusDegreePos)
+  venus.position.x = coords['x']
+  venus.position.z = coords['y']
+}
+
+const earthOrbit = () => {
+  if (earthDegreePos === 360){
+    earthDegreePos = 0
+  } else {
+    earthDegreePos += .1
+  }
+  let coords = convertCoord(55,earthDegreePos)
+  earth.position.x = coords['x']
+  earth.position.z = coords['y']
+}
+
+const marOrbit = () => {
+  if (marDegreePos === 360){
+    marDegreePos = 0
+  } else {
+    marDegreePos += .07
+  }
+  let coords = convertCoord(70,marDegreePos)
+  mars.position.x = coords['x']
+  mars.position.z = coords['y']
+}
+
+const jupiterOrbit = () => {
+  if (jupiterDegreePos === 360){
+    jupiterDegreePos = 0
+  } else {
+    jupiterDegreePos += .35
+  }
+  let coords = convertCoord(100,jupiterDegreePos)
+  jupiter.position.x = coords['x']
+  jupiter.position.z = coords['y']
+}
+
+const saturnOrbit = () => {
+  if (saturnDegreePos === 360){
+    saturnDegreePos = 0
+  } else {
+    saturnDegreePos += .13
+  }
+  let coords = convertCoord(135,saturnDegreePos)
+  saturn.position.x = coords['x']
+  saturn.position.z = coords['y']
+}
+
+const uranusOrbit = () => {
+  if (uranusDegreePos === 360){
+    uranusDegreePos = 0
+  } else {
+    uranusDegreePos += .25
+  }
+  let coords = convertCoord(165,uranusDegreePos)
+  uranus.position.x = coords['x']
+  uranus.position.z = coords['y']
+}
+
+const neptuneOrbit = () => {
+  if (neptuneDegreePos === 360){
+    neptuneDegreePos = 0
+  } else {
+    neptuneDegreePos += .05
+  }
+  let coords = convertCoord(195,neptuneDegreePos)
+  neptune.position.x = coords['x']
+  neptune.position.z = coords['y']
+}
+
+
 function animate(){
   requestAnimationFrame(animate);
-
+  mecuryOrbit()
+  venusOrbit()
+  earthOrbit()
+  marOrbit()
+  jupiterOrbit()
+  saturnOrbit()
+  uranusOrbit()
+  neptuneOrbit()
   renderer.render(scene,camera);
 }
 
 animate()
+
